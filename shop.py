d007b76bf543ab9c122fc1bdd70f96b545531eed
Denis Khmelnov <denishmelnov@gmail.com>
2022-10-14 21:07:57 +0500

    feat: implement Store class

--- shop.py
+++ shop.py
@@ -1,14 +1,23 @@
-from storage import Storage
+from store import Store
 
+class Shop(Store):
 
-class Store(Storage):
     def __init__(self):
         self._items = {}
-        self._capacity = 100
+        self._capacity = 20
+        self._items_count = 0
 
     @property
     def items(self):
         return self._items
+
+    @property
+    def items_count(self):
+        return self._items_count
+
+    @items_count.setter
+    def items_count(self, value):
+        self._items_count = value
 
     @property
     def capacity(self):
@@ -21,12 +30,16 @@ class Store(Storage):
 
     def add(self, title, qnt):
         if self.capacity == 0:
-            print("На складе нет места")
+            print("В магазине нет места")
         elif self.get_free_space() >= qnt:  # проверяем, что места достаточно
             if self.items.get(title):
                 self.items[title] += qnt
             else:
+                if self.items_count == 5:
+                    print("В магазине не может быть больше 5 видов товара")
+                    return False
                 self.items[title] = qnt  # добавляем новый товар
+                self.items_count += 1
             self.capacity -= qnt  # уменьшаем свободное место
         else:  # если места на весь товар недостаточно
             if self.items.get(title):
@@ -37,11 +50,12 @@ class Store(Storage):
 
     def remove(self, title, qnt):
         if self.items.get(title):
-            if self.items[title] >= qnt:
+            if self.items[title] > qnt:
                 self.items[title] -= qnt
                 self.capacity += qnt
             else:
-                self.capacity += self.items.pop(title) #обнуляем количество товара и удаляем его из словаря
+                self.capacity += self.items.pop(title)
+                self.items_count -= 1
         else:
             print("Нет такого товара")
 
@@ -53,3 +67,10 @@ class Store(Storage):
 
     def get_unique_items_count(self):
         return len(self.items)
+
+
+shop = Shop()
+shop.add("cocain", 5)
+shop.add("heroin", 3)
+shop.remove("heroin", 3)
+print(shop.items_count)
